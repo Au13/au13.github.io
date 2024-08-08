201ab0bbc0000d05493bec5906593d5a792617f6
AU199 <275011@sycamoreschools.org>
2024-08-08 16:35:04 -0400

    testing

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let next_button = document.getElementById('submit');
 
 next_button.addEventListener('click',function(){
    console.log("clicked button"); 
+   console.log(document.getElementsByName("Swerve"));
+   console.log(document.getElementsByName("Swerve"));
+   console.log(document.getElementsByName("Swerve"));
+   console.log(document.getElementsByName("Swerve"));
 });
 
 
